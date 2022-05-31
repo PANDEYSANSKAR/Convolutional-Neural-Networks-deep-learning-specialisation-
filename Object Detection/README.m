2f6e9f1baf541f7edfd29cd9fe2c1eab8831bd0c
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:25:01 +0530

    Update README.m

--- Object Detection/README.m
+++ Object Detection/README.m
@@ -1,1 +1,25 @@
 
+# Autonomous Driving - Car Detection
+
+Welcome to the Week 3 programming assignment! In this notebook, you'll implement object detection using the very powerful YOLO model. Many of the ideas in this notebook are described in the two YOLO papers: [Redmon et al., 2016](https://arxiv.org/abs/1506.02640) and [Redmon and Farhadi, 2016](https://arxiv.org/abs/1612.08242). 
+
+**By the end of this assignment, you'll be able to**:
+
+- Detect objects in a car detection dataset
+- Implement non-max suppression to increase accuracy
+- Implement intersection over union
+- Handle bounding boxes, a type of image annotation popular in deep learning
+
+# Image Segmentation with U-Net
+
+Welcome to the final assignment of Week 3! You'll be building your own U-Net, a type of CNN designed for quick, precise image segmentation, and using it to predict a label for every single pixel in an image - in this case, an image from a self-driving car dataset. 
+
+This type of image classification is called semantic image segmentation. It's similar to object detection in that both ask the question: "What objects are in this image and where in the image are those objects located?," but where object detection labels objects with bounding boxes that may include pixels that aren't part of the object, semantic image segmentation allows you to predict a precise mask for each object in the image by labeling each pixel in the image with its corresponding class. The word “semantic” here refers to what's being shown, so for example the “Car” class is indicated below by the dark blue mask, and "Person" is indicated with a red mask:
+As you might imagine, region-specific labeling is a pretty crucial consideration for self-driving cars, which require a pixel-perfect understanding of their environment so they can change lanes and avoid other cars, or any number of traffic obstacles that can put peoples' lives in danger. 
+
+By the time you finish this notebook, you'll be able to: 
+
+* Build your own U-Net
+* Explain the difference between a regular CNN and a U-net
+* Implement semantic image segmentation on the CARLA self-driving car dataset
+* Apply sparse categorical crossentropy for pixelwise prediction
