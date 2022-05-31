491ca52f1ace0205115a7ae837a990e10373b329
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:19:45 +0530

    Update README.m

--- Deep Convolutional Models: Case Studies/README.m
+++ Deep Convolutional Models: Case Studies/README.m
@@ -1,1 +1,21 @@
+# Residual Networks
 
+Welcome to the first assignment of this week! You'll be building a very deep convolutional network, using Residual Networks (ResNets). In theory, very deep networks can represent very complex functions; but in practice, they are hard to train. Residual Networks, introduced by [He et al.](https://arxiv.org/pdf/1512.03385.pdf), allow you to train much deeper networks than were previously feasible.
+
+**By the end of this assignment, you'll be able to:**
+
+- Implement the basic building blocks of ResNets in a deep neural network using Keras
+- Put together these building blocks to implement and train a state-of-the-art neural network for image classification
+- Implement a skip connection in your network
+
+
+# Transfer Learning with MobileNetV2
+
+A pre-trained model is a network that's already been trained on a large dataset and saved, which allows you to use it to customize your own model cheaply and efficiently. The one you'll be using, MobileNetV2, was designed to provide fast and computationally efficient performance. It's been pre-trained on ImageNet, a dataset containing over 14 million images and 1000 classes.
+
+By the end of this assignment, you will be able to:
+
+- Create a dataset from a directory
+- Preprocess and augment data using the Sequential API
+- Adapt a pretrained model to new data and train a classifier using the Functional API and MobileNet
+- Fine-tune a classifier's final layers to improve accuracy
