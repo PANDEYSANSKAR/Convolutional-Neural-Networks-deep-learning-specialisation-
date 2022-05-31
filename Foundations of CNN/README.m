f53848999e4a759c4260c16a14100caf239cb7af
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:16:20 +0530

    Update README.m

--- Foundations of CNN/README.m
+++ Foundations of CNN/README.m
@@ -1,1 +1,23 @@
+# Convolutional Neural Networks: Step by Step
 
+Welcome to Course 4's first assignment! In this assignment, you will implement convolutional (CONV) and pooling (POOL) layers in numpy, including both forward propagation and (optionally) backward propagation. 
+
+By the end of this notebook, you'll be able to: 
+
+* Explain the convolution operation
+* Apply two different types of pooling operation
+* Identify the components used in a convolutional neural network (padding, stride, filter, ...) and their purpose
+* Build a convolutional neural network 
+
+# Convolutional Neural Networks: Application
+
+Welcome to Course 4's second assignment! In this notebook, you will:
+
+- Create a mood classifer using the TF Keras Sequential API
+- Build a ConvNet to identify sign language digits using the TF Keras Functional API
+
+**After this assignment you will be able to:**
+
+- Build and train a ConvNet in TensorFlow for a __binary__ classification problem
+- Build and train a ConvNet in TensorFlow for a __multiclass__ classification problem
+- Explain different use cases for the Sequential and Functional APIs
